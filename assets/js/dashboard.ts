cb791b0ee25a2868dd953364a83868146a2ad859
Kacper Rogowski <rogemus@gmail.com>
2024-02-07 13:24:40 +0100

    main Add picnic css, and remove old custom css

--- assets/js/dashboard.ts
+++
@@ -1,7 +0,0 @@
-import { transactionsPerCategoryChart } from './charts/transactionsPerCategoryChart';
-import { monthFlowChart } from './charts/monthFlowChart'
-
-(async () => {
-  transactionsPerCategoryChart();
-  monthFlowChart();
-})()
