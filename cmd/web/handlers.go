482d3abfcf5ca2c06c474a972c3a1207b37f119e
Kacper Rogowski <rogemus@gmail.com>
2024-03-28 08:49:23 +0100

    main: Add basic error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,34 +3,46 @@ package web
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"tracker/pkg/models"
+	"tracker/pkg/utils"
 )
-
-type ResBody struct {
-	Data string
-}
-
-func GetHello(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	hello := ResBody{Data: "hello"}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(hello)
-}
 
 func (a *App) GetBudget(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	id, _ := strconv.Atoi(r.PathValue("id"))
+	budget, err := a.Database.GetBudget(id)
 
-	// TODO: handle error
-	budget, _ := a.Database.GetBudget(1)
+	// TODO handle different type of error
+	// TODO write tests
+	if err != nil {
+		errMsg := err.Error()
+		errPayload := models.ErrorPayload{Msg: errMsg}
+		utils.LogError(errMsg)
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(errPayload)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(budget)
 }
 
 func (a *App) GetBudgets(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	budgets, err := a.Database.GetBudgets()
 
-	// TODO: handle error
-	budgets, _ := a.Database.GetBudgets()
+	// TODO handle different type of error
+	// TODO write tests
+	if err != nil {
+		errMsg := err.Error()
+		errPayload := models.ErrorPayload{Msg: errMsg}
+		utils.LogError(errMsg)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errPayload)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(budgets)
 }
---
+++ pkg/utils/log.go
@@ -0,0 +1,15 @@
+package utils
+
+import "log"
+
+func LogError(msg string) {
+	log.Printf("[ERROR] %s", msg)
+}
+
+func LogInfo(msg string) {
+	log.Printf("[ERROR] %s", msg)
+}
+
+func LogWarning(msg string) {
+	log.Printf("[ERROR] %s", msg)
+}
