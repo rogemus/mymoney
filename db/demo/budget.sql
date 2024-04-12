a62bdd9eea4013f89c0d3507122ad302d6870297
Kacper Rogowski <rogemus@gmail.com>
2024-04-12 14:05:48 +0200

    Add demo content sql

---
+++ db/demo/budget.sql
@@ -0,0 +1,6 @@
+INSERT INTO budget
+    (Title, Description, UserID)
+VALUES
+    ('ARCHITAX', 'Elit dolor cillum elit aute do aliquip esse. Nostrud id eu ut labore eiusmod non.', 1),
+    ('FUTURITY', 'In et consequat sit tempor in sit laboris. Qui amet eiusmod minim labore.', 1),
+    ('CYTREX', '', 1);
