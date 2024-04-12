0f58f5b1b8117853d0b529a085ce56a134f8466f
Kacper Rogowski <rogemus@gmail.com>
2024-04-12 14:36:31 +0200

    Update demo

--- db/demo/user.sql
+++ db/demo/user.sql
@@ -1,4 +1,4 @@
 INSERT INTO user
-    (Username, Email, Password)
+    (ID, Username, Email, Password)
 VALUES
-  ("test", "test@test.com", "$2a$14$K4TVJJ43ddGnXZ/65J4EyOwGtgTx6UWjDyxmyhPqXWI0qhg0kGXty")
+    (1, "test", "test@test.com", "$2a$14$K4TVJJ43ddGnXZ/65J4EyOwGtgTx6UWjDyxmyhPqXWI0qhg0kGXty")
