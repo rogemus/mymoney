df645fa48b6a0723aa7ce0f5d32492fa69c70658
Kacper Rogowski <rogemus@gmail.com>
2024-04-12 14:36:31 +0200

    Update demo

--- db/demo/budget.sql
+++ db/demo/budget.sql
@@ -1,6 +1,6 @@
 INSERT INTO budget
-    (Title, Description, UserID)
+    (ID, Title, Description, UserID)
 VALUES
-    ('ARCHITAX', 'Elit dolor cillum elit aute do aliquip esse. Nostrud id eu ut labore eiusmod non.', 1),
-    ('FUTURITY', 'In et consequat sit tempor in sit laboris. Qui amet eiusmod minim labore.', 1),
-    ('CYTREX', '', 1);
+    (1, 'ARCHITAX', 'Elit dolor cillum elit aute do aliquip esse. Nostrud id eu ut labore eiusmod non.', 1),
+    (2, 'FUTURITY', 'In et consequat sit tempor in sit laboris. Qui amet eiusmod minim labore.', 1),
+    (3, 'CYTREX', '', 1);
