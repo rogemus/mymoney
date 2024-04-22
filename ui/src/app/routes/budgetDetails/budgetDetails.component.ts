27b3d18d80d2277a2a995ee9b0bb6fece214239a
Kacper Rogowski <rogemus@gmail.com>
2024-04-22 16:21:44 +0200

    Setup Angular 17

---
+++ ui/src/app/routes/budgetDetails/budgetDetails.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'budgetDetails-page',
+  templateUrl: './budgetDetails.component.html',
+  styleUrl: './budgetDetails.component.css'
+})
+export class BudgetDetailsPage { }
---
+++ ui/src/app/routes/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginPage { }
